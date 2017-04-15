de9852a12eef39d4c5d38277139e9d4efe0fc95b
Daniel Demetroulis <ddemetroullis@gmail.com>
2017-04-15 17:28:26 -0600

    addition of compile.py to easily run gulp commands

---
+++ compile.py
@@ -0,0 +1,10 @@
+#! python
+import sys
+import os
+import subprocess
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+subprocess.check_call(dir_path + '/node_modules/.bin/gulp copy', shell=True)
+subprocess.check_call(dir_path + '/node_modules/.bin/gulp react', shell=True)
+subprocess.check_call(dir_path + '/node_modules/.bin/gulp sass', shell=True)
