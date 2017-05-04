3f1115ef8e45e3ee050ff3d1c9da899e9790971e
Daniel Demetroulis <ddemetroulis@tlcdelivers.com>
2017-05-04 17:47:21 -0600

    reporting php unit tests to console during generation

--- unit_test.py
+++ unit_test.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import json
 import glob
+import re
 
 '''
 Handles the running of php unit tests
@@ -18,8 +19,40 @@ tests = []
 for test in test_paths:
     tests.append(test.replace('\\', '/').split('/').pop().replace('.php', ''))
 
+#generate the results
 for test in tests:
     output = subprocess.Popen( 'php ' + dir_path + '/WebContent/index.php tests ' + test, stdout=subprocess.PIPE ).communicate()[0]
     file = open(dir_path+'/unittests/'+test+'.html', 'w')
     file.truncate()
     file.write(str(output,'utf-8'));
+    file.close()
+
+# scan and report the results
+for test in tests:
+     file = open(dir_path+'/unittests/'+test+'.html', 'r')
+     result = False
+     note = False
+     resultSet = {}
+
+     for line in file:
+         if result:
+             resultSet[test] = {}
+             if 'Passed' in line:
+                 resultSet[test]['passed'] = 'Passed'
+             else:
+                 resultSet[test]['passed'] = 'Failed'
+         elif note:
+             resultSet[test]['function'] = re.split('<|>', line)[2]
+         if 'Result</th>' in line:
+             result = True
+         else:
+             result = False
+         if 'Notes</th>' in line:
+            note = True
+         else:
+            note = False
+
+#results
+for key in resultSet.keys():
+    print(key + ': ' + resultSet[key]['function'] + ' = ' + resultSet[key]['passed'])
+    
