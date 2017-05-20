b63d1ee75886ddf479917d87de298e84e9574bc0
Daniel Demetroulis <ddemetroullis@gmail.com>
2017-05-20 16:24:16 -0600

    fixing issues by bad packages

--- install.py
+++ install.py
@@ -7,6 +7,8 @@ import glob
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+if not os.path.exists(dir_path+'/composer'):
+    os.makedirs(dir_path+'/composer')
 # npm install
 os.chdir(dir_path)
 subprocess.Popen('npm install', stdout=subprocess.PIPE, shell=True )
