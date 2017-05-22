a44f6fa4300e1f0fa990c15e1dcc65e117b6493c
Daniel Demetroulis <ddemetroulis@tlcdelivers.com>
2017-05-22 17:34:28 -0600

    added signup files

---
+++ WebContent/application/language/english/signup_lang.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['username'] = 'username';
+$lang['email'] = 'email';
+$lang['email_confirm'] = 'confirm email';
+$lang['first_name'] = 'first name';
+$lang['last_name'] = 'last name';
+$lang['birthdate'] = 'Birthdate';
+$lang['signup'] = 'Sign Up';
+$lang['back'] = '< Go Back';
---
+++ WebContent/application/language/spanish/signup_lang.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['username'] = 'nombre de usuario';
+$lang['email'] = 'correo electrónico';
+$lang['email_confirm'] = 'confirmar correo electrónico';
+$lang['first_name'] = 'nombre de pila';
+$lang['last_name'] = 'apellido';
+$lang['birthdate'] = 'Fecha de nacimiento';
+$lang['signup'] = 'Abre una cuenta';
+$lang['back'] = '< Regresa';
